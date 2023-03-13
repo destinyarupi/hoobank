8cefac222a107187d1668a2a171d84bc634b384a
Destiny Arupi <destinyarupi@gmail.com>
2023-03-12 20:45:37 -0600

    all done

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants/index'
+
+const NavBar = () => {
+  const [toggle, setToggle] = useState(false)
+  return (
+    <nav className='w-full flex justify-between items-center navbar py-6'>
+      <img src={logo} alt="HooBank" className='w-[124px] h-[32px]'/>
+      <div>
+        <ul className='list-none sm:flex hidden justify-end items-center'>
+          {navLinks.map((nav, index) =>
+            <li
+              key={nav.id}
+              className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
+            >
+              <a href={`#${nav.title}`}>{nav.title}</a>
+            </li>
+          )}
+        </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img src={toggle ? close : menu}
+            alt="hamburger-menu"
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(prevState => !prevState)}
+          />
+          <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+            <ul className='list-none flex flex-col justify-end items-center'>
+              {navLinks.map((nav, index) =>
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${index === navLinks.length - 1 ? 'mr-0' : 'mb-10'}`}
+                >
+                  <a href={`#${nav.title}`}>{nav.title}</a>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
